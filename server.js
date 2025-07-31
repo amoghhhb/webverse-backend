e73ca86b6be354a9fa102c7468abb0f6a0c465c6
amoghhhb <5070875@bkbck.edu.in>
2025-07-31 13:31:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:3002',
   'http://localhost:3003',
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'http://127.0.0.1:5501/'
 ];
 
 const corsOptions = {
